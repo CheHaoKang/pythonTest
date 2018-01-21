8ec06cab928de28ddddc067dacafa4fe912a6182
Decken Kang <deckenkang66@blueplanet.com.tw>
2018-01-21 21:37:47 +0800

    revision

--- getInstitutionalInvestors.py
+++ getInstitutionalInvestors.py
@@ -5,6 +5,7 @@ import sys
 import datetime
 from fake_useragent import UserAgent
 import json
+import time
 
 def getInstitutionalInvestors(date):
 	# 自營商買賣超彙總表
@@ -36,10 +37,12 @@ def getInstitutionalInvestors(date):
 					insInvestArray.append((hyphenDate, data[0].strip(), 'dealer', 'dealer', data[8].strip().replace(",", "")))
 					insInvestArray.append((hyphenDate, data[0].strip(), 'dealer', 'dealer', ('' if data[9].strip().replace(",", "")=='0' else '-') + data[9].strip().replace(",", "")))
 				succeed = True
-			elif '很抱歉' in s['stat']:
+			elif 'stat' in s and '很抱歉' in s['stat']:
 				succeed = True
 		except:
 			print("Unexpected error:", sys.exc_info())
+
+	time.sleep(1)
 
 	# 投信買賣超彙總表
 	succeed = False
@@ -54,10 +57,12 @@ def getInstitutionalInvestors(date):
 					insInvestArray.append((hyphenDate,data[1].strip(),' investmentTrust',' investmentTrust',data[3].strip().replace(",", "")))
 					insInvestArray.append((hyphenDate, data[1].strip(), ' investmentTrust', ' investmentTrust', ('' if data[4].strip().replace(",", "")=='0' else '-') + data[4].strip().replace(",", "")))
 				succeed = True
-			elif '很抱歉' in s['stat']:
+			elif 'stat' in s and '很抱歉' in s['stat']:
 				succeed = True
 		except:
 			print("Unexpected error:", sys.exc_info())
+
+	time.sleep(1)
 
 	# 外資及陸資買賣超彙總表
 	succeed = False
@@ -72,25 +77,27 @@ def getInstitutionalInvestors(date):
 					insInvestArray.append((hyphenDate, data[1].strip(),'foreignInvestor','foreignInvestor',data[3].strip().replace(",", "")))
 					insInvestArray.append((hyphenDate, data[1].strip(),'foreignInvestor','foreignInvestor',('' if data[4].strip().replace(",", "") == '0' else '-') + data[4].strip().replace(",", "")))
 				succeed = True
-			elif '很抱歉' in s['stat']:
+			elif 'stat' in s and '很抱歉' in s['stat']:
 				succeed = True
 		except:
 			print("Unexpected error:", sys.exc_info())
 
 	if insInvestArray:  # not empty
-		try:
-			conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='89787198',
-								   db='stockevaluation', charset="utf8")
-			cur = conn.cursor()
-			insert = "INSERT INTO stockInstitutionalInvestor (stockDate, stockCode, stockInvestorType, stockInvestorTypeDetail, stockAmount) VALUES (%s, %s, %s, %s, %s)"
-			cur.executemany(insert, insInvestArray)
-			cur.close()
-			conn.commit()
-			conn.close()
-		except:
-			print("Unexpected error:", sys.exc_info())
-			cur.close()
-			conn.close()
+		while True:
+			try:
+				conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='89787198',
+									   db='stockevaluation', charset="utf8")
+				cur = conn.cursor()
+				insert = "INSERT INTO stockInstitutionalInvestor (stockDate, stockCode, stockInvestorType, stockInvestorTypeDetail, stockAmount) VALUES (%s, %s, %s, %s, %s)"
+				cur.executemany(insert, insInvestArray)
+				cur.close()
+				conn.commit()
+				conn.close()
+				break
+			except:
+				print("Unexpected error:", sys.exc_info())
+				cur.close()
+				conn.close()
 
 if __name__ == "__main__":
 	getInstitutionalInvestors(str(sys.argv[1]))
